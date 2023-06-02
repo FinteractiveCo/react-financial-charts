eed9acc23a23078fa09a3b40d3e8734e4c2b7f94
Ján Mertel <10438157+jancimertel@users.noreply.github.com>
2023-06-02 11:00:02 +0200

    add datacenter lib package + example

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -4,13 +4,23 @@ import { useLocalStorage } from "@mantine/hooks";
 import {
   ColorScheme,
   ColorSchemeProvider,
-  Loader,
   MantineProvider,
 } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { AppProps } from "next/app";
 import "../styles/app.css";
+import DatacenterClient, { Resolution } from "datacenter-js-client";
+
+const apiClient = new DatacenterClient("https://api-atc.finteractive.co/dev");
+apiClient
+  .getCandles({
+    symbol: "EURUSD",
+    resolution: Resolution.M5,
+    fromTimestamp: 1685577600,
+    toTimestamp: 1685664000,
+  })
+  .then(console.log.bind(undefined, "lol"));
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
